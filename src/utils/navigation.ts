1635559aab8595ee6921293b9cf97d8e9b202982
Cristian Montes <cmontesmujica@gmail.com>
2026-04-27 00:49:32 -0600

    adjusted template per hawthron scapes branding

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,26 +1,27 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: 'Home', url: '/' },
-  { name: 'Services', url: '/#services' },
+  { name: 'About', url: '/about' },
+  { name: 'Services', url: '/services' },
   { name: 'Contact', url: '/#contact' },
 ];
 // An array of links for footer
 const footerLinks = [
   {
-    section: 'Services',
-    links: [
-      { name: 'Lawn & Maintenance', url: '/services' },
-      { name: 'Installation & Design', url: '/services' },
-      { name: 'Hardscape & Structures', url: '/services' },
-    ],
-  },
-  {
     section: 'Company',
     links: [
-      { name: 'About', url: '/' },
+      { name: 'About', url: '/about' },
+      { name: 'Services', url: '/services' },
       { name: 'Service Area', url: '/#service-area' },
       { name: 'FAQ', url: '/#faq' },
       { name: 'Contact', url: '/#contact' },
+    ],
+  },
+  {
+    section: 'Contact',
+    links: [
+      { name: 'Email', url: 'mailto:info@hawthornscapes.com' },
+      { name: 'Phone', url: 'tel:7208491565' },
     ],
   },
 ];
